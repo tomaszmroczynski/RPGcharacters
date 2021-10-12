3f9f792bc3482969a124e633943e0f070b88347a
Tomasz Mroczynski <tomaszmroczynski@gmail.com>
2021-10-12 22:18:35 +0200

    attributes created, next step specify it to each class

---
+++ RPGchar/Char.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGchar
+{
+    class Char  
+
+    {
+        public string name;
+
+        public int level;   
+
+        public int strength;
+
+        public int dexterity;
+
+        public int inteligence;
+
+        public int vitality;
+
+        public int health;
+
+        public int elementalResistance;
+
+        public int armorRating;
+
+        public int damageLevel;
+    }
+}
+}
---
+++ RPGchar/Ranger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGchar
+{
+    class Ranger
+    {
+        public string Name { get; set; }
+
+        public int Level { get; set; }
+
+        public int Strength { get; set; }
+
+        public int Dexterity { get; set; }
+        public int Inteligence { get; set; }
+        public int Vitality { get; set; }
+        public int Health
+        {
+            get { return health; }
+            set { health = vitality * 10 + value; }
+        }
+
+        public int ArmorRating
+        {
+            get { return armorRating; }
+            set { armorRating = dexterity + strength + value; }
+        }
+
+        public int ElemetalResistance
+        {
+            get { return elementalResistance; }
+            set { elementalResistance = Inteligence + value; }
+        }
+
+
+
+        public int DamageLevel
+        {
+            get { return damageLevel; }
+            set { elementalResistance = inteligence; }
+        }
+    }
+}
